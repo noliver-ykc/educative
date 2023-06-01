fb09def5dfdfb0df091c155bc9ea3c9b95adc3bb
noliver-ykc <noliver.ykc@gmail.com>
2023-06-01 00:50:06 +0000

    Solved

---
+++ Grokking the Coding interview in Javascript/isPalindrome.js
@@ -0,0 +1,21 @@
+function isPalindrome(str){
+    let start = 0;
+    let end = str.length-1;
+    let mid = end/2;
+  
+    while(start < mid) {
+      if(str[start] === str[end]){
+        
+        start += 1;
+        end -= 1;
+      } else {
+        return false;
+      }
+    }
+    return true;
+  
+  }
+  
+  
+  console.log(isPalindrome("bob")) // true;
+  console.log(isPalindrome("RACEACAR")) // false;
